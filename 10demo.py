882dd1e7de07ce40bebffbc7d109ac12c9670a4e
Thy Trinh <thytrinh@Thys-MacBook-Air.local>
2026-02-17 20:43:28 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -1,10 +1,22 @@
 print('hello world')
 
-# 10demo.py by your name
+# 10demo.py by Thy Trinh
 print('hello, again')
 
+"""
+This is a 
+multiline comment
+"""
+import math
 
 #Math 
 print(1.5e-2)
 
+print(1+1)
+print(2**3)
+print(pow(2,3))
+print(math.pow(2,3))
+print(2**0.5)
+print(math.sqrt(2))
+print(math.log(2))
 
