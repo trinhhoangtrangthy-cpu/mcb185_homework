3173518d6cc59e1a95030305ae403d15668b3da4
Thy Trinh <thytrinh@Thys-MacBook-Air.local>
2026-02-17 21:40:57 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -20,3 +20,27 @@ print(2**0.5)
 print(math.sqrt(2))
 print(math.log(2))
 
+print(0.1*1)
+print(0.1*3)
+
+#Variables
+a = 3
+b = 4
+c = math.sqrt(a**2 + b**2)
+print(c)
+
+#Type
+print(type(a), type(b), type(c))
+print(type(a), type(b), type(c), sep=',', end='!\n')
+
+#Functions
+def pythagoras(a, b):
+    c = math.sqrt(a**2 + b**2)
+    return c
+
+hyp = pythagoras(3,4)
+print(hyp)
+
+def pythagoras(a, b):
+    return math.sqrt(a**2 + b**2)
+print(pythagoras(3,4))
